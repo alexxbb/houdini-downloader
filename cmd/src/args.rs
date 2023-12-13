bfac5e6c69927fba23185808396dbdb139f33305
Aleksei Rusev <hou.alexx@gmail.com>
2023-12-12 17:10:37 -0800

    Small tweaks

---
+++ cmd/src/args.rs
@@ -0,0 +1,107 @@
+use clap::{Parser, Subcommand, ValueEnum};
+use houdini_downloader_api::{Platform, Product};
+use std::ops::Not;
+use std::path::PathBuf;
+
+/// Utility for downloading SideFX Houdini installers.
+#[derive(Debug, Parser)]
+#[clap(disable_help_subcommand = true)]
+#[command(author, version)]
+pub struct Args {
+    #[command(subcommand)]
+    pub commands: Commands,
+    #[arg(long, global = true, env = "SESI_USER_ID", hide_env_values = true)]
+    pub user_id: Option<String>,
+    #[arg(long, global = true, env = "SESI_USER_SECRET", hide_env_values = true)]
+    pub user_secret: Option<String>,
+    #[arg(long, global = true, value_enum, default_value_t = ProductArg::Houdini)]
+    pub product: ProductArg,
+    #[arg(long, global = true, value_enum, default_value_t = PlatformArg::Linux)]
+    pub platform: PlatformArg,
+}
+
+impl Args {
+    pub fn parse_() -> Self {
+        Args::parse()
+    }
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Download a particular build.
+    Get {
+        /// Product version [e.g. 19.5]
+        #[arg(short, long)]
+        version: String,
+
+        /// Product build number.
+        #[arg(short, long)]
+        build: u64,
+
+        /// Directory to save the downloaded file.
+        #[arg(short, long)]
+        output_dir: PathBuf,
+
+        /// Skip download confirmation question.
+        #[arg(short, long)]
+        auto_confirm: bool,
+
+        /// Overwrite if file exists in the output directory.
+        #[arg(long)]
+        overwrite: bool,
+    },
+    /// List available builds.
+    List {
+        /// By default, only production builds are listed.
+        #[arg(short, long, default_value_t = false)]
+        include_daily_builds: bool,
+        /// Product version [e.g. 19.5]
+        #[arg(short, long)]
+        version: String,
+    },
+}
+
+impl Commands {
+    pub fn is_version_valid(&self) -> bool {
+        // Verify if version is major.minor
+        let version = match self {
+            Commands::Get { version, .. } => version,
+            Commands::List { version, .. } => version,
+        };
+        version.ends_with('.').not() && version.split('.').count() == 2
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, ValueEnum)]
+pub enum ProductArg {
+    Houdini,
+    HoudiniLauncher,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, ValueEnum)]
+pub enum PlatformArg {
+    Linux,
+    Win64,
+    Macos,
+    MacosxArm64,
+}
+
+impl From<ProductArg> for Product {
+    fn from(arg: ProductArg) -> Self {
+        match arg {
+            ProductArg::Houdini => Product::Houdini,
+            ProductArg::HoudiniLauncher => Product::HoudiniLauncher,
+        }
+    }
+}
+
+impl From<PlatformArg> for Platform {
+    fn from(arg: PlatformArg) -> Self {
+        match arg {
+            PlatformArg::Linux => Platform::Linux,
+            PlatformArg::Win64 => Platform::Win64,
+            PlatformArg::Macos => Platform::Macos,
+            PlatformArg::MacosxArm64 => Platform::MacosxArm64,
+        }
+    }
+}
--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,115 +1,37 @@
+mod args;
+
+use crate::args::{Args, Commands};
 use anyhow::{bail, Context, Result};
-use clap::{Parser, Subcommand, ValueEnum};
 use dialoguer::{theme::ColorfulTheme, Confirm};
 use futures_util::StreamExt;
-use houdini_downloader_api::{Platform, Product, SesiClient};
+use houdini_downloader_api::SesiClient;
 use indicatif::ProgressStyle;
 use md5::{Digest, Md5};
 use owo_colors::{AnsiColors, OwoColorize};
 use std::io::Write;
-use std::path::PathBuf;
 use tokio::io::AsyncWriteExt;
-
-/// Utility for downloading SideFX Houdini installers.
-#[derive(Debug, Parser)]
-struct App {
-    #[command(subcommand)]
-    commands: Commands,
-    #[arg(long, global = true, env = "SESI_USER_ID", hide_env_values = true)]
-    user_id: Option<String>,
-    #[arg(long, global = true, env = "SESI_USER_SECRET", hide_env_values = true)]
-    user_secret: Option<String>,
-    #[arg(long, global = true, value_enum, default_value_t = ProductArg::Houdini)]
-    product: ProductArg,
-    #[arg(long, global = true, value_enum, default_value_t = PlatformArg::Linux)]
-    platform: PlatformArg,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// Download a particular build.
-    Get {
-        /// Product version [e.g. 19.5]
-        #[arg(short, long)]
-        version: String,
-
-        /// Product build number.
-        #[arg(short, long)]
-        build: u64,
-
-        /// Directory to save the downloaded file.
-        #[arg(short, long)]
-        output_dir: PathBuf,
-
-        /// Skip download confirmation question.
-        #[arg(short, long)]
-        auto_confirm: bool,
-
-        /// Overwrite if file exists in the output directory.
-        #[arg(long)]
-        overwrite: bool,
-    },
-    /// List available builds.
-    List {
-        /// By default, only production builds are listed.
-        #[arg(short, long, default_value_t = false)]
-        include_daily_builds: bool,
-        /// Product version [e.g. 19.5]
-        #[arg(short, long)]
-        version: String,
-    },
-}
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq, ValueEnum)]
-enum ProductArg {
-    Houdini,
-    HoudiniLauncher,
-}
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq, ValueEnum)]
-enum PlatformArg {
-    Linux,
-    Win64,
-    Macos,
-    MacosxArm64,
-}
-
-impl From<ProductArg> for Product {
-    fn from(arg: ProductArg) -> Self {
-        match arg {
-            ProductArg::Houdini => Product::Houdini,
-            ProductArg::HoudiniLauncher => Product::HoudiniLauncher,
-        }
-    }
-}
-
-impl From<PlatformArg> for Platform {
-    fn from(arg: PlatformArg) -> Self {
-        match arg {
-            PlatformArg::Linux => Platform::Linux,
-            PlatformArg::Win64 => Platform::Win64,
-            PlatformArg::Macos => Platform::Macos,
-            PlatformArg::MacosxArm64 => Platform::MacosxArm64,
-        }
-    }
-}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let args: App = App::parse();
+    let args: Args = Args::parse_();
 
     if args.user_id.is_none() || args.user_secret.is_none() {
         bail!("SESI_USER_ID and SESI_USER_SECRET are required");
     }
 
-    ctrlc::set_handler(move || {
-        std::process::exit(0);
-    })
-    .context("Error setting up CTRL-C handler")?;
+    if !args.commands.is_version_valid() {
+        bail!("Version number must be major.minor [e.g 19.5]")
+    }
 
     // None variants were checked above
     let user_id = args.user_id.as_deref().unwrap();
     let user_secret = args.user_secret.as_deref().unwrap();
+
+    ctrlc::set_handler(move || {
+        println!("Killed with CTRL-C");
+        std::process::exit(0);
+    })
+    .context("Error setting up CTRL-C handler")?;
 
     let client = SesiClient::new(user_id, user_secret)
         .await
@@ -146,8 +68,8 @@ async fn main() -> Result<()> {
                 .await
                 .context("Could not send GET download request")?;
             let mut stream = response.bytes_stream();
-            let pb = indicatif::ProgressBar::new(build_info.size);
-            pb.set_style(
+            let bar = indicatif::ProgressBar::new(build_info.size);
+            bar.set_style(
                 ProgressStyle::default_bar()
                     .template(
                         "{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] \
@@ -155,7 +77,7 @@ async fn main() -> Result<()> {
                     )?
                     .progress_chars("#>-"),
             );
-            pb.set_message(format!("Downloading {}", filename));
+            bar.set_message(format!("Downloading {}", filename));
             let mut file = tokio::fs::File::create(&output)
                 .await
                 .context("Could not create file to save")?;
@@ -166,11 +88,11 @@ async fn main() -> Result<()> {
                         .await
                         .context("Error writing to output file")?;
                     hash.update(&bytes);
-                    pb.inc(bytes.len() as u64);
+                    bar.inc(bytes.len() as u64);
                 }
             }
-            pb.finish_with_message(format!("Downloaded: {}", output.to_string_lossy()));
-            let downloaded_bytes_hash = hex::encode(&hash.finalize()[..]);
+            bar.finish_with_message(format!("Downloaded: {}", output.to_string_lossy()));
+            let downloaded_bytes_hash = hex::encode(&hash.finalize());
             if downloaded_bytes_hash != build_info.hash {
                 eprintln!(
                     "{}",
@@ -211,7 +133,6 @@ async fn main() -> Result<()> {
                     build.release
                 )?;
             }
-            drop(stdout);
         }
     }
 
